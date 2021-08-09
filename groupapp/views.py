60451fee305ad8e8585cc3064a18ad75416806b5
IlyosRustamov <rustamov7871@gmail.com>
2021-08-09 20:20:28 +0500

    utkirbek uchun

--- groupapp/views.py
+++ groupapp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from .models import Group, Person
 from .serializer import GroupSerializer, PersonSerializer
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class GroupListView(generics.ListCreateAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    #FIRST
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class GroupDetailView(generics.RetrieveUpdateDestroyAPIView):
